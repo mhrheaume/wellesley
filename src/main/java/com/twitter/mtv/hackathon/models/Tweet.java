28cf0072777a24b610f59bc88d06703a950fb8b3
Emily Ahn <eahn@wellesley.edu>
2016-02-06 16:03:21 -0500

    change templates to display Hillary's retweet statuses

--- src/main/java/com/twitter/mtv/hackathon/models/Tweet.java
+++ src/main/java/com/twitter/mtv/hackathon/models/Tweet.java
@@ -1,12 +1,17 @@
 package com.twitter.mtv.hackathon.models;
 
 import twitter4j.Status;
+// new class: User
+import twitter4j.User;
 
 public class Tweet {
     private Status tweet;
+    // new var
+    private User user;
 
     public Tweet(Status tweet) {
         this.tweet = tweet;
+        this.user = tweet.getUser();
     }
 
     public long id() { return tweet.getId(); }
@@ -14,6 +19,18 @@ public class Tweet {
     public String username() { return tweet.getUser().getScreenName(); }
 
     public String text() { return tweet.getText(); }
+
+    //adding to Status
+    public String isRetweeted() { 
+        if (tweet.isRetweet()) {
+            return "http://bizbest.wpengine.netdna-cdn.com/wp-content/uploads/2012/12/retweet.jpg";
+        } else { return "https://pbs.twimg.com/profile_images/1221566601/Sad_Bird.jpg"; } 
+    }
+
+    //get url to profile picture
+    public String getProfPicID() {
+        return user.getMiniProfileImageURLHttps();
+    }
 
     public String toString() {
         return this.text();
